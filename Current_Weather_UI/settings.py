3c1ef73b614e072252dc5f1d4f991cb84f6607a3
Polymath-Saksh <67389168+Polymath-Saksh@users.noreply.github.com>
2024-02-24 10:04:11 +0530

    static correct

--- Current_Weather_UI/settings.py
+++ Current_Weather_UI/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jzo35q--uy41_%nagyr$50_@d6rd7owfdw6*vb!^k=g10ljw-w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,7 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/' # This is the URL where Django will look for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+] # This is the directory where Django will look for static files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
