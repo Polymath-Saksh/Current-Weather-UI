268f56e75230849f7bd4a18c46c3371c9d3798a8
Saksham Kumar <sakshamkumarprasad03@gmail.com>
2024-03-21 23:25:41 +0530

    Django 2103

--- weather/weather.py
+++ weather/weather.py
@@ -1,6 +1,7 @@
 from weather.secret import get_key
 from weather.geocoding import city_to_geocoding
 import requests
+from datetime import datetime
 
 # The program utilizes the OpenWeatherMap API to get the weather data
 
@@ -18,7 +19,7 @@ def program(location):
         exit()
 
     # Get the URL for the Weather API call
-    base_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=alerts,minutely,hourly,daily&appid={API_KEY}"
+    base_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={API_KEY}&units=metric"
 
     # Get the response from the API call
     response = requests.get(base_url)
@@ -26,20 +27,65 @@ def program(location):
     # Convert the response to JSON format
     x = response.json()
     # If the "current" key exists in the response, retrieve the weather data
-    if "current" in x:
+    if x:
         # Get the current data from the JSON response
         current_data = x["current"]
         current_temperature = current_data["temp"]
         current_pressure = current_data["pressure"]
         current_humidity = current_data["humidity"]
-        weather_description = current_data["weather"][0]["description"]
-
+        weather_description = current_data["weather"][0]["main"]
+        cloudiness = current_data["clouds"]
+        visibility = current_data["visibility"]
+        wind_speed = current_data["wind_speed"]
+        wind_direction = current_data["wind_deg"]
+        max_temp = x["daily"][0]["temp"]["max"]
+        min_temp = x["daily"][0]["temp"]["min"]
+        #Capture sunrise and sunset and convert to 00:00 in 24 hour format
+        sunrise = datetime.fromtimestamp(x["daily"][0]["sunrise"])
+        sunset = datetime.fromtimestamp(x["daily"][0]["sunset"])
+        curr_date = date_converter(str(sunrise).split()[0])
         output = {
             "temp": current_temperature,
             "pressure": current_pressure,
             "humidity": current_humidity,
-            "description": weather_description
+            "description": weather_description,
+            "cloudiness": cloudiness,
+            "visibility": visibility/1000,
+            "wind_speed": wind_speed,
+            "wind_direction": wind_direction,
+            "max_temp": max_temp,
+            "min_temp": min_temp,
+            "sunrise": str(sunrise).split()[1][:5],
+            "sunset": str(sunset).split()[1][:5],
+            "curr_date": f'{curr_date[1]} {curr_date[0]}',
+            "year": curr_date[2]
             }
         return output
     else:
         return None
+def date_converter(date):
+    dat = date.split('-')
+    day = dat[-1]
+    mth = dat[-2]
+    yr = dat[-3]
+    months = {
+        "01": "JAN",
+        "02": "FEB",
+        "03": "MAR",
+        "04": "APR",
+        "05": "MAY",
+        "06": "JUN",
+        "07": "JUL",
+        "08": "AUG",
+        "09": "SEP",
+        "10": "OCT",
+        "11": "NOV",
+        "12": "DEC"
+    }
+    return [day, months[mth], yr]
+
+def day_night(sunrise,sunset,current_time):
+    if sunrise < current_time < sunset:
+        return "Day"
+    else:
+        return "Night"
